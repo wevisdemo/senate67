1483f8e9498b0afb14404512cf936f8983f22f55
Th1nkK1D <witheep@gmail.com>
2024-03-12 17:00:22 +0700

    feat: config font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		fontFamily: {
+			sarabun: ["Sarabun", ...defaultTheme.fontFamily.sans],
+			crushual: ["Crushual", ...defaultTheme.fontFamily.serif],
+			trirong: ["Trirong", ...defaultTheme.fontFamily.serif],
+		},
 		fontSize: [],
 		colors: [],
 	},
