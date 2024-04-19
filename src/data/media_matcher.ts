0973639f914c35f288a2136d591cadace3c0d508
Th1nkK1D <witheep@gmail.com>
2024-04-19 11:48:43 +0700

    fix: allow build when video link json is not exist

--- src/data/media_matcher.ts
+++ src/data/media_matcher.ts
@@ -1,15 +1,18 @@
-import { readdirSync, readFileSync } from "fs";
+import { readdirSync, readFileSync, existsSync } from "fs";
 import { join } from "path";
 
 const AVATAR_PATH = "candidate-media/images";
 const DEFAULT_AVATAR = "/example-candidate-image.svg";
 
-const VIDEO_LINKS_PATH = "candidate-media/video-links.json";
+const VIDEO_LINKS_JSON = join(
+	process.cwd(),
+	"public/candidate-media/video-links.json",
+);
 
 const existingAvatar = readdirSync(join(process.cwd(), "public", AVATAR_PATH));
-const videoLinks = JSON.parse(
-	readFileSync(join(process.cwd(), "public", VIDEO_LINKS_PATH)).toString(),
-);
+const videoLinks = existsSync(VIDEO_LINKS_JSON)
+	? JSON.parse(readFileSync(VIDEO_LINKS_JSON).toString())
+	: [];
 
 export function getAvatarUrl(firstName: string, lastName: string) {
 	const expectedAvatar = existingAvatar.find((filename) =>
